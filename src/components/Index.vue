0d95c12f1b10b6d492e0c115a90987a13ca49f47
Daniel Loureiro Cintra <cintra.70@gmail.com>
2017-11-03 18:53:52 -0200

    adição do mapa

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,5 @@
 <script>
-
+	
 	import { map } from 'lodash'
 
 	export default{
--- src/components/mapa/main.vue
+++ src/components/mapa/main.vue
@@ -1,9 +1,17 @@
 <script>
+	
+	import Vue from 'vue'
+	import * as VueGoogleMaps from 'vue2-google-maps'
+
+	Vue.use(VueGoogleMaps, {
+	  load: {
+	    key: 'AIzaSyDVQ6DTwqyg83ozYfnVCA9mFxijuIj516k'
+	  }
+	})
 
 	export default{
 		name:"Mapa",
 		components: {
-
 		},
 		data(){
 			return {
@@ -17,11 +25,19 @@
 </script>
 
 <template>	
-	<div class="container">
-		<h1>Aqui vai entrar o mapa</h1>
+	<div>
+		<div slot="header" class="toolbar">
+			<q-toolbar-title :padding="0">
+				Onde vamos entregar seu pedido?
+			</q-toolbar-title>
+		</div>
+		<gmap-map :center="{lat:-19.9026612, lng:-44.1044812}" :zoom="7" map-type-id="terrain" class="google-map"></gmap-map>
 	</div>
 </template>
 
 <style scoped>
-	
+	.google-map{
+		width: 100vw;
+		height: 100vh;
+	}
 </style>
